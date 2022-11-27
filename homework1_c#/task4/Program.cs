a661a33833e8d711526839f3713c596cdf502eac
Daria <macdaria1999@gmail.com>
2022-11-27 18:32:45 +0300

    4

---
+++ homework1_c#/task4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    if (a > c)
+    
+        Console.Write($"max = {a}");
+    
+    else 
+    
+        Console.Write($"max = {c}");
+else if (b > c)
+    Console.Write($"max = {b}");
+else 
+    Console.Write($"max = {c}");
+
+
