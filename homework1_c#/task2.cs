12f4ab9f73d4402f604877796b69f62b3ec7180b
Daria <macdaria1999@gmail.com>
2022-11-27 18:15:04 +0300

    task2

---
+++ homework1_c#/task2.cs
@@ -0,0 +1,10 @@
+﻿
+// task 2
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (m > n)
+    Console.Write($"max = {m}");
+else 
+    Console.Write($"max = {n}");
